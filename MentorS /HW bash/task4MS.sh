9ef19b147000b4ef1a7c25db2b804b119a6fc743
Polina Nesterovich <nesterovich.p.a@gmail.com>
2025-11-01 19:01:33 +0200

    Delete MentorS  directory

--- MentorS /HW bash/task4MS.sh
+++
@@ -1,9 +0,0 @@
-read -p "Введите имя файла: " filename
-if [ ! -f "$filename" ]; then
-    echo "Ошибка: Файл '$filename' не существует!"
-    exit 1
-fi
-
-line_count=$(wc -l < "$filename")
-
-echo "Количество строк в файле '$filename': $line_count"
