f63e5a82fc4262dc2d4a1f11e4f37ac8368442aa
Polina Nesterovich <nesterovich.p.a@gmail.com>
2025-11-01 19:02:48 +0200

    Delete MS  directory

--- MS /HW bash/task3MS.sh
+++
@@ -1,6 +0,0 @@
-read -p "Введите путь к директории для архивирования: " source_dir
-current_date=$(date +%Y-%m-%d)
-dir_name=$(basename "$source_dir")
-archive_name="${dir_name}_backup_${current_date}.tar.gz"
-tar -czf "$archive_name" -C "$(dirname "$source_dir")" "$(basename "$source_dir")"
-echo "Архив создан: $archive_name"
